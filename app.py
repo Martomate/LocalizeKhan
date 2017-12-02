ff8d7565306e5c3f76f6c6376a4b8d2242a69744
alexandertu <alexander.tuoma@gmail.com>
2017-12-02 02:52:53 +0100

    Add git add .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
+    <h1>Hello heroku!!!</h1>
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400">
